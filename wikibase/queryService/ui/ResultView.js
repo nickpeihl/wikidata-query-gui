e69740a148e6ffb555846f1eb8beb3ae896f0e1e
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2017-10-13 04:41:52 -0400

    Init implementation of the map editor
    * Used some code from http://osmose.openstreetmap.fr/en/map/#
    
    Change-Id: I9318e2d16af8b5d35c3185d00b0578b5390d11d3

--- wikibase/queryService/ui/ResultView.js
+++ wikibase/queryService/ui/ResultView.js
@@ -95,6 +95,13 @@ wikibase.queryService.ui.ResultView = ( function( $, window ) {
 			object: null,
 			$element: null
 		},
+		Editor: {
+			icon: 'glyphicon-map-marker',
+			label: 'Map Editor',
+			class: 'EditorResultBrowser',
+			object: null,
+			$element: null
+		},
 		Map: {
 			icon: 'glyphicon-map-marker',
 			label: 'Map',
